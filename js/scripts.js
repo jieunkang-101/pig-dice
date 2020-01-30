03a916bf2d00fc06fc80cde5a6b400c6d256c211
Jieun Kang and Kristina Hengster Tintor <jieun@>
2020-01-30 10:18:34 -0800

    fix total score logic

--- js/scripts.js
+++ js/scripts.js
@@ -32,68 +32,30 @@ $(document).ready(function () {
     $("#player2name").text(player2.name);
   });
 
+  
   $("#rollBtn").click(function(event) {
     event.preventDefault();
     var diceNum = Math.floor(Math.random() * 6 + 1);
     $("#player1-dice").text(diceNum);
-      if (diceNum !== 1) {
-        player1.getCurrentScore(diceNum);
-        $("#player1-current-score").text(player1.currentScore);
-      } else {
-        currentScore = 0;
-        player1.getCurrentScore(diceNum);
-        player1.getTotalScore(currentScore);
-        $("#player1-current-score").text(player1.currentScore);
-        $("#player1-total-score").text(player1.totalScore);
-      }
-    });
+    if (diceNum !== 1) {
+      player1.getCurrentScore(diceNum);
+      $("#player1-current-score").text(player1.currentScore);
+    } else {
+      currentScore = 0;
+      player1.getCurrentScore(diceNum);
+      player1.getTotalScore(currentScore);
+      $("#player1-current-score").text(player1.currentScore);
+      $("#player1-total-score").text(player1.totalScore);
+    }
   });
-
   $("#holdBtn").click(function(event){
     event.preventDefault();
-    // var diceNum = Math.floor(Math.random() * 6 + 1);
-    
-    // player1.totalScore += currentScore;
+    currentScore = player1.currentScore;
+    player1.getTotalScore(this.currentScore);
     $("#player1-total-score").text(player1.totalScore);
-    currentScore = 0;
-    $("#player1-current-score").text(player1.currentScore);
-    console.log(palyer1.currentScore);
-    console.log(player1.totalScore);
-   });
+  });
+});
 
 
 
-//         $("#holdBtn").click(function (event) {
-//           event.preventDefault();
-//           player1.totalScore += currentScore;
-//           $("#player1-total-score").text(player1.totalScore);
-//           currentScore = 0;
-//           turn = 1;
-//         });
-//       } else {
-//         currentScore = 0;
-//         $(".current-score").text(currentScore);
-//         turn = 1;
-//       }
-      
-//     } else {
-//       var diceNum = Math.floor(Math.random() * 6 + 1);
-//       $("#player2-dice").text(diceNum);
-//       if (diceNum !== 1) {
-//         currentScore += diceNum;
-//         $("#player2-current-score").text(currentScore);
-//         $("#holdBtn").click(function (event) {
-//           event.preventDefault();
-//           player2.totalScore += currentScore;
-//           $("#player2-total-score").text(player2.totalScore);
-//           currentScore = 0;
-//           turn = 0;
-//           });
-//       } else {
-//           currentScore = 0;
-//           $(".current-score").text(currentScore);
-//           turn=0;
-//         } 
-//     };
- 
-// });
+
